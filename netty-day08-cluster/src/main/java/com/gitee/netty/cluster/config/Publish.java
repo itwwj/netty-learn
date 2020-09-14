93a2761668d47135152cf108e4a2d8cf7c4fbde0
王文杰 <275236367@qq.com>
2020-09-14 11:17:58 +0800

    netty服务集群注册中心优化

--- netty-day08-cluster/src/main/java/com/gitee/netty/cluster/config/Publish.java
+++ netty-day08-cluster/src/main/java/com/gitee/netty/cluster/config/Publish.java
@@ -1,9 +1,11 @@
 package com.gitee.netty.cluster.config;
 
+import com.alibaba.fastjson.JSON;
 import com.gitee.netty.cluster.model.MsgAgreement;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 
@@ -16,10 +18,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class Publish {
     @Autowired
-    private RedisTemplate redisTemplate;
+    private StringRedisTemplate redisTemplate;
 
     public void pushMessage(String topic, MsgAgreement message) {
         log.info("向 "+topic+"发送消息："+message);
-        redisTemplate.convertAndSend(topic, message);
+        redisTemplate.convertAndSend(topic, JSON.toJSONString(message));
     }
 }
